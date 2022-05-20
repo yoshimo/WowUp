a6d75a6aea5960d10a40b07deaeea40702e58632
Grant Pannell <300992+DigitalDJ@users.noreply.github.com>
2022-05-20 18:11:27 +0930

    Re-enable CurseForge Addon Providers

--- wowup-electron/src/app/services/wowup/patch-notes.service.ts
+++ wowup-electron/src/app/services/wowup/patch-notes.service.ts
@@ -24,8 +24,6 @@ const CHANGELOGS: ChangeLog[] = [
     </ul>
     <h4 style="margin-top: 1em;">Changes</h4>
     <ul>
-    <li>Curse addon provider removed</li>
-    <li>CurseV2 addon provider removed</li>
     <li>GitHub update check should now attempt to use release.json</li>
     <li>GitHub should now respect release channels better</li>
     <li>Spanish locale updates (SkollVargr)</li>
